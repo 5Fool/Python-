c0dc49df86c94d08a4f5a7220ec2489e248baaa3
5Fool <tuchongjing@qq.com>
2026-06-07 10:12:05 +0800

    删除 魔术方法.py

--- 魔术方法.py
+++
@@ -1,8 +0,0 @@
-class cu:
-    def __init__(self,ct):
-        self.ct=ct
-    def __getitem__(self,cr):
-        return self.ct[cr]
-cc=cu(["a","b","c","d","e","f","g"])
-for i in cc:
-    print(i)
